c71f513bf2d3529cf1e4d9fbf0f9def86b4d151e
besufkad-ayele <ayebesufkad@gmail.com>
2023-02-28 19:43:22 +0300

    message

--- 0x05-pointers_arrays_strings/100-atoi-working.c
+++ 0x05-pointers_arrays_strings/100-atoi-working.c
@@ -1,36 +1,29 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _atoi - Convert a string to integer.
- * @s: char array string
- * Return: first integer found in string
+ * _atoi - Entry point
+ * Description: Convert String to Integer
+ * @s: Integer
+ * Return: char
  */
 
 int _atoi(char *s)
 {
-	int i;
-	int h, p;
+	int sign = 1;
+	unsigned int ans = 0;
 
-	h = 0;
-	p = -1;
-	for (i = 0; s[i] != '\0'; i++)
+	while (*s != '\0' && (*s < '0' || *s > '9'))
 	{
-		if (s[i] == '-')
-			p *= -1;
-
-		if (s[i] > 47 && s[i] < 58)
+		if (*s == '-')
 		{
-			if (h < 0)
-				h = (h * 10) - (s[i] - '0');
-			else
-				h = (s[i] - '0') * -1;
-
-			if (s[i + 1] < 48 || s[i + 1] > 57)
-				break;
+			sign *= -1;
 		}
+		s++;
 	}
-	if (p < 0)
-		h *= -1;
-
-	return (h);
+	while (*s >= '0' && *s <= '9')
+	{
+		ans = ans * 10 + (*s - '0');
+		s++;
+	}
+	return (sign * ans);
 }
